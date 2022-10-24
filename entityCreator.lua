6d6d3f51c8729ea2cc87a0ef736b8a30e64d9593
spongus <112719635+sponguss@users.noreply.github.com>
2022-10-24 01:01:39 -0300

    Update entityCreator.lua

--- entityCreator.lua
+++ entityCreator.lua
@@ -1,7 +1,7 @@
 local lp=game:GetService"Players".LocalPlayer
 local char=lp.Character or lp.CharacterAdded:Wait()
 local hum=char:FindFirstAncestorOfClass("Humanoid")
-local root=hum.RootPart
+local root=hum.HumanoidRootPart
 
 local DefaultConfig={
     speed = 1,
