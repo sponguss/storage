48fadea7f790d777dcf5813ceabe4708d2210ca8
spongus <112719635+sponguss@users.noreply.github.com>
2022-10-24 01:20:22 -0300

    Update entityCreator.lua

--- entityCreator.lua
+++ entityCreator.lua
@@ -60,7 +60,7 @@ return function(config)
             end)
         end
     end)
-    config.speed=75/100*config.Speed
+    config.speed=75/100*config.speed
     config.code.onEntityConfig(config)
 
     pcall(writefile, "customentity.txt", game:HttpGet(config.entityModel))
