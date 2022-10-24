2c5d358215f46b092e312c6011c0a0c1df45a2f3
spongus <112719635+sponguss@users.noreply.github.com>
2022-10-24 13:16:32 -0300

    Update entityCreator.lua

--- entityCreator.lua
+++ entityCreator.lua
@@ -69,7 +69,8 @@ return function(config)
     local entityModel=game:GetObjects((type(config.entityModel)=="string" and (getcustomasset or getsynasset)("customentity.txt") or "rbxassetid://"..tostring(config.entityModel)))[1]
     config.code.onModelWritingDone(entityModel)
     if entityModel:IsA("BasePart") then local temp=Instance.new("Model", game:GetService("Teams")); temp.Name=entityModel.Name; entityModel.Parent=temp; entityModel=temp end
-    local entityRoot=entityModel.PrimaryPart or entityModel:FindFirstChildWhichIsA("BasePart")
+    if not entityModel.PrimaryPart then entityModel.PrimaryPart=entityModel:FindFirstChildWhichIsA("BasePart") end
+    local entityRoot=entityModel.PrimaryPart-- or entityModel:FindFirstChildWhichIsA("BasePart")
     entityRoot.Anchored=true; entityRoot.CanCollide=false;
 
     local room_l = workspace.CurrentRooms[tostring(game:GetService("ReplicatedStorage").GameData.LatestRoom.Value)]
